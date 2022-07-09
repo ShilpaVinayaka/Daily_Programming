a056d354396f8e8cfd596d844e1b255d92e994a3
shilpa_v <55580071+ShilpaVinayaka@users.noreply.github.com>
2022-07-09 21:38:09 +0530

    Update BurstBalloonPoints.cpp

--- Hard/BurstBalloonPoints.cpp
+++ Hard/BurstBalloonPoints.cpp
@@ -1,3 +1,9 @@
+/*
+given N balloons, each with a number of coins associated with it. On bursting a balloon i, the number of coins gained is 
+equal to A[i-1]*A[i]*A[i+1]. Also, balloons i-1 and i+1 now become adjacent. Find the maximum possible profit earned after 
+bursting all the balloons. Assume an extra 1 at each boundary. 
+*/
+
 #include <bits/stdc++.h>
 #include <iostream>
 using namespace std;
