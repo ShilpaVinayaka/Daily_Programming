76e44753186a37a625f39a1c4a00c3c76cce6345
shilpa_v <55580071+ShilpaVinayaka@users.noreply.github.com>
2022-07-13 23:24:12 +0530

    Create ChildSumProperty_BinaryTree.cpp

---
+++ Easy/ChildSumProperty_BinaryTree.cpp
@@ -0,0 +1,85 @@
+/* Program to check children sum property 
+
+Given a binary tree, write a function that returns true if the tree satisfies below property. 
+For every node, data value must be equal to sum of data values in left and right children. Consider data value as 0 for NULL children. Below tree is an example
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+/* A binary tree node has data, left
+child and right child */
+struct node
+{
+	int data;
+	struct node* left;
+	struct node* right;
+};
+
+/* returns 1 if children sum property holds
+for the given node and both of its children*/
+int isSumProperty(struct node* node)
+{
+	/* left_data is left child data and
+	right_data is for right child data*/
+	int sum = 0;
+	
+	/* If node is NULL or it's a leaf node
+	then return true */
+	if(node == NULL ||
+		(node->left == NULL &&
+		node->right == NULL))
+		return 1;
+	else
+	{
+		/* If left child is not present then 0
+		is used as data of left child */
+		if(node->left != NULL)
+		sum += node->left->data;
+	
+		/* If right child is not present then 0
+		is used as data of right child */
+		if(node->right != NULL)
+		sum += node->right->data;
+	
+		/* if the node and both of its children
+		satisfy the property return 1 else 0*/
+		return ((node->data == sum)&&
+			isSumProperty(node->left) &&
+			isSumProperty(node->right));
+	}
+}
+
+/*
+Helper function that allocates a new node
+with the given data and NULL left and right
+pointers.
+*/
+struct node* newNode(int data)
+{
+	struct node* node =
+		(struct node*)malloc(sizeof(struct node));
+	node->data = data;
+	node->left = NULL;
+	node->right = NULL;
+	return(node);
+}
+
+// Driver Code
+int main()
+{
+	struct node *root = newNode(10);
+	root->left	 = newNode(8);
+	root->right = newNode(2);
+	root->left->left = newNode(3);
+	root->left->right = newNode(5);
+	root->right->right = newNode(2);
+	if(isSumProperty(root))
+		cout << "The given tree satisfies "
+			<< "the children sum property ";
+	else
+		cout << "The given tree does not satisfy "
+			<< "the children sum property ";
+
+	getchar();
+	return 0;
+}
