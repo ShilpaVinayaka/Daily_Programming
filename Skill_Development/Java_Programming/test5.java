dea834543e7d69ad05b4a380ec292bb573450e14
shilpa_vinayaka <55580071+ShilpaVinayaka@users.noreply.github.com>
2022-08-25 19:51:06 +0530

    Create test5.java

---
+++ Skill_Development/Java_Programming/test5.java
@@ -0,0 +1,16 @@
+public class Main
+{
+    public static void main(String [] args) 
+    {
+         int x = 10;
+         int y=11;
+ 
+         do { 
+             
+         } while (x++ < y);
+         System.out.println(x);
+    }
+}
+
+// 12
+
