2798b02b68912ee25b2e94efee89b93604f1dba1
shilpa_vinayaka <55580071+ShilpaVinayaka@users.noreply.github.com>
2022-09-09 18:46:10 +0530

    Create test4.java

---
+++ Skill_Development/Java_Programming/Intro_to_Java/test4.java
@@ -0,0 +1,19 @@
+public class Main
+{
+	public static void main(String[] args) 
+	{
+		for(int i = 0; i < 3; i++) 
+        { 
+            switch(i) 
+            { 
+                case 0: break; 
+                case 1: System.out.print("one "); 
+                case 2: System.out.print("two "); 
+                case 3: System.out.print("three "); 
+            } 
+        } 
+        System.out.println("done");
+	}
+}
+
+// one two three two three done
