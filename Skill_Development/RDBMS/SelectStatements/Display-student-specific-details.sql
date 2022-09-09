c6ae6c87b5bd13b07ab0e3605f137613121fa4cc
shilpa_vinayaka <55580071+ShilpaVinayaka@users.noreply.github.com>
2022-09-09 11:54:20 +0530

    Create Display-student-specific-details.sql

---
+++ Skill_Development/RDBMS/SelectStatements/Display-student-specific-details.sql
@@ -0,0 +1,5 @@
+---Write a query to display all the student names and their city. Sort the results by name in ascending order.
+
+  SELECT student_name, city
+  FROM Student
+  ORDER BY student_name ASC;
