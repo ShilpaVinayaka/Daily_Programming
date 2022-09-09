0189f63a2a0b55b92d2c3c716c8c58ad1ad45a6a
shilpa_vinayaka <55580071+ShilpaVinayaka@users.noreply.github.com>
2022-09-09 18:44:14 +0530

    Create labels_in_java.java

---
+++ Skill_Development/Java_Programming/Intro_to_Java/labels_in_java.java
@@ -0,0 +1,20 @@
+public class Main
+{
+	public static void main(String[] args) {
+		
+		boolean isTrue = true;
+		outer:
+		    for(int i=0; i<5; i++)
+		    {
+		        while(isTrue)
+		        {
+		            System.out.println("Hello");
+		            break outer;   //Will break out of the loop and label
+		        }
+		        System.out.println("Outer loop"); // Won't print
+		    }
+		    System.out.println("Bye Bye");
+	}
+}
+
+// Output: 2
