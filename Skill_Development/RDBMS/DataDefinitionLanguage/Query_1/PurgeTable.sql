57802eae482fffbbc8f009b6010e1c1b331995f9
shilpa_vinayaka <55580071+ShilpaVinayaka@users.noreply.github.com>
2022-09-09 11:42:50 +0530

    Create PurgeTable.sql

---
+++ Skill_Development/RDBMS/DataDefinitionLanguage/Query_1/PurgeTable.sql
@@ -0,0 +1,2 @@
+---The resort management wants to purge all the records from the "Town" table.  Write an SQL statement to purge all the records from the Town table without allowing rollback activity.
+TRUNCATE TABLE Town;
