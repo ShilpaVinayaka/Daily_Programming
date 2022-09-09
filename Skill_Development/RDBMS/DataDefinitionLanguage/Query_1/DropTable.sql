d88a57e406e01c6f49b5792c2333d2f025f372c4
shilpa_vinayaka <55580071+ShilpaVinayaka@users.noreply.github.com>
2022-09-09 11:43:38 +0530

    Create DropTable.sql

---
+++ Skill_Development/RDBMS/DataDefinitionLanguage/Query_1/DropTable.sql
@@ -0,0 +1,6 @@
+---Refer the following schema and drop the Course table.
+
+---Hint: To drop the parent table, all associate tables need to be dropped or Use 'cascade constraints' command
+
+DROP TABLE REGISTRATION;
+DROP TABLE COURSE;
