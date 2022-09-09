480db23da1c0cfd66ba90b9b7a43c3da008723c7
shilpa_vinayaka <55580071+ShilpaVinayaka@users.noreply.github.com>
2022-09-09 11:42:18 +0530

    Create AlterTable1.sql

---
+++ Skill_Development/RDBMS/DataDefinitionLanguage/Query_1/AlterTable1.sql
@@ -0,0 +1,5 @@
+--- The resort management wants to change the name of the table "Town" to "Municipality". Write a SQL statement to fulfill the requirement.
+
+---[Hint : Use alter statement to rename the table]
+
+ALTER TABLE Town RENAME TO Municipality;
