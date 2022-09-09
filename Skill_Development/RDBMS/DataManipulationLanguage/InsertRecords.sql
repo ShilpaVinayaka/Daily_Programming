a606bd5fec7ed3c4243cf2ef3122632c3ad54eed
shilpa_vinayaka <55580071+ShilpaVinayaka@users.noreply.github.com>
2022-09-09 11:50:26 +0530

    Create InsertRecords.sql

---
+++ Skill_Development/RDBMS/DataManipulationLanguage/InsertRecords.sql
@@ -0,0 +1,8 @@
+---Insert the below records into Payments table.
+
+---PAYMENT_ID Ticket_ID BD_ID DISCOUNT_ID
+---PT1          TT5      1005        DT1
+---PT2          TT6      1004        DT2
+
+INSERT INTO Payments VALUES ('PT1', 'TT5', 1005, 'DT1'); 
+INSERT INTO Payments VALUES ('PT2', 'TT6', 1004, 'DT2');
