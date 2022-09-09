d814f836fc1d41a3274ef1c1ea6de2299da1c9fc
shilpa_vinayaka <55580071+ShilpaVinayaka@users.noreply.github.com>
2022-09-09 11:40:47 +0530

    Update AlterTable2.sql

--- Skill_Development/RDBMS/DataDefinitionLanguage/Query_1/AlterTable2.sql
+++ Skill_Development/RDBMS/DataDefinitionLanguage/Query_1/AlterTable2.sql
@@ -1,1 +1,2 @@
+---Refer the following schema and add a constraint CHECK_FEES in course table to ensure that the fee is more than five hundred.
 ALTER TABLE COURSE ADD CONSTRAINT CHECK_FEES CHECK(Fees >500);
