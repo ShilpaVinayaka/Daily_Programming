d02356a6d71b5c7aef68589549e32b5d9f1caa03
shilpa_vinayaka <55580071+ShilpaVinayaka@users.noreply.github.com>
2022-09-09 12:09:29 +0530

    Create Display-resort-rating.sql

---
+++ Skill_Development/RDBMS/Function-Scalar-and-Aggregate/Display-resort-rating.sql
@@ -0,0 +1,3 @@
+---Select the highest & lowest rating of the resort and display the output as "HIGHEST RATING" and "LOWEST RATING".
+SELECT MAX(starRating) AS "HIGHEST RATING", MIN(starRating) AS "LOWEST RATING"
+FROM Resort;
