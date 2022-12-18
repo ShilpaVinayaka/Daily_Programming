96b9d3b54e34495d0597dd844a9a2099365b69e5
shilpa_vinayaka <55580071+ShilpaVinayaka@users.noreply.github.com>
2022-12-18 13:34:18 +0530

    Create Stack_reverse.java

---
+++ Consistency_Programming/Basics/Stack_reverse.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+public class Stack_reverse {
+    public static void pushAtTheBottom(int data, Stack<Integer> s) {
+        if (s.isEmpty()) {
+            s.push(data);
+            return;
+        }
+        int top = s.pop();
+        pushAtTheBottom(data, s);
+        s.push(top);
+    }
+
+    public static void reverse(Stack<Integer> s) {
+        if (s.isEmpty()) {
+            return;
+        }
+        int top = s.pop();
+        reverse(s);
+        pushAtTheBottom(top, s);
+    }
+
+    public static void main(String[] args) {
+        Stack<Integer> s = new Stack<>();
+        s.push(1);
+        s.push(2);
+        s.push(3);
+
+        reverse(s);
+
+        while (!s.isEmpty()) {
+            System.out.println(s.peek());
+            s.pop();
+        }
+    }
+}
